03c96c28a0b89dccb9cd44e6623a2f9f67521515
tiagomsmota <tiagomota95@gmail.com>
2018-09-11 00:35:48 +0100

    Setup CRUD for Job model

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const express       = require("express"),
-      app           = express(),
-      bodyParser    = require("body-parser"),
-      mongoose      = require("mongoose"),
-      seedDatabase  = require("./seeds");
+const express        = require("express"),
+      app            = express(),
+      bodyParser     = require("body-parser"),
+      methodOverride = require("method-override"),
+      mongoose       = require("mongoose"),
+      seedDatabase   = require("./seeds");
 
 app.set("view engine", "ejs");
 app.use(express.static("assets"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 //----database setup - mongoose
 mongoose.connect("mongodb://localhost:27017/rocket-jobs", {useNewUrlParser: true});
@@ -37,6 +39,27 @@ app.get("/jobs/new", function(req, res) {
 });
 
 //create
+app.post("/jobs", function(req,res) {
+    function convertString(str) {
+        var string = str.replace(/\w\S*/g, function(x) { 
+          return x.charAt(0).toUpperCase() + x.substr(1);
+        });
+        var convertedString = string.replace(/\s+/g, "");
+        return convertedString.split(",");
+    }
+    var jobSkills = convertString(req.body.job.skills);
+    var jobData = {company: req.body.job.company, title: req.body.job.title, location: req.body.job.location, regimen: req.body.job.regimen, description: req.body.job.description, benefits: req.body.job.benefits, skills: jobSkills};
+    Job.create(jobData, function(err, createdJob) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("A new job has been added:");
+            console.log(createdJob);
+            createdJob.save();
+            res.redirect("/jobs");
+        }
+    });
+});
 
 //show
 app.get("/jobs/:id", function(req, res) {
@@ -50,11 +73,41 @@ app.get("/jobs/:id", function(req, res) {
 });
 
 //edit
+app.get("/jobs/:id/edit", function(req, res) {
+    Job.findById(req.params.id, function(err, foundJob) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("editjob", {job : foundJob});
+            console.log(foundJob.benefits);
+        }
+    });
+});
 
 //update
+app.put("/jobs/:id", function(req, res) {
+    Job.findByIdAndUpdate(req.params.id, req.body.job, function(err, updatedJob) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("The following job has been updated: " + updatedJob.title);
+            console.log(req.body);
+            res.redirect("/jobs/" + req.params.id);
+        }
+    });
+});
 
 //destroy
-
+app.delete("/jobs/:id", function(req, res) {
+    Job.findByIdAndDelete(req.params.id, function(err, deletedJob) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("The following job has been deleted:" + deletedJob.title);
+            res.redirect("/jobs");
+        }
+    });
+});
 
 
 //----server setup
